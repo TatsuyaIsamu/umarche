140455c471a4d8cb84796ecc04a88c375d023e96
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-12-25 15:00:08 +0900

    pagination

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -32,6 +32,21 @@ class OwnerSeeder extends Seeder
             'password' => Hash::make('password123'),
             'created_at' => '2021/01/01 11:11:11'
             ],
+            [            'name' => 'test',
+            'email' => 'test4@test.com',
+            'password' => Hash::make('password123'),
+            'created_at' => '2021/01/01 11:11:11'
+            ],
+            [            'name' => 'test',
+            'email' => 'test5@test.com',
+            'password' => Hash::make('password123'),
+            'created_at' => '2021/01/01 11:11:11'
+            ],
+            [            'name' => 'test',
+            'email' => 'test6@test.com',
+            'password' => Hash::make('password123'),
+            'created_at' => '2021/01/01 11:11:11'
+            ],
         ]);
     }
 }
