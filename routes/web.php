fc0d75e972b369a57a3dbe6b220661ab516f5e58
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-12-28 20:46:48 +0900

    user 商品一覧

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
-
+use App\Http\Controllers\User\ItemController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,10 +24,12 @@ Route::get('/component-test1', [ComponentTestController::class, 'showComponent1'
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 
+Route::middleware('auth:users')->group(function(){
+        Route::get('/', [ItemController::class, 'index'])->name('items.index');
+    });
 
-
-Route::get('/dashboard', function () {
-    return view('user.dashboard');
-})->middleware(['auth:users'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('user.dashboard');
+// })->middleware(['auth:users'])->name('dashboard');
 
 require __DIR__.'/auth.php';
