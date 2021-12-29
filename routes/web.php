214276116a6a26771b8ae5de9baa77afbcb63949
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-12-29 13:39:59 +0900

    item show view

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServic
 
 Route::middleware('auth:users')->group(function(){
         Route::get('/', [ItemController::class, 'index'])->name('items.index');
+        Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
     });
 
 // Route::get('/dashboard', function () {
